e86340292d6454f402dd5687d9a57ac26bae8646
katerinbarrera <katerinbarrera2001@gmail.com>
2024-03-17 22:02:55 -0500

    changing route path

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '',
     element: <Principal />,
     errorElement: <ErrorPage />,
   },
